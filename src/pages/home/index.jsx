96f4b74df35edcd230b97cfac06ebcfbe305a0fb
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2023-07-27 15:13:15 -0300

    Add button and header to project

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
 
 const Home = () => {
     return (<>
-        <h1>Home</h1>
+        <Header />
+        <Button title={'teste1'}></Button>
+        <Button title={'teste1'} variant='secondary'>Teste1</Button>
         <Link to="/login">Fazer Login</Link>
     </>
     );
